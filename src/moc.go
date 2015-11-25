f96e4ee49fa52d93f925466add66a52e1986f1af
qianlifeng <qianlf2008@163.com>
2015-11-26 00:24:06 +0800

    add marked lib

--- src/moc.go
+++ src/moc.go
@@ -2,15 +2,17 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
 )
 
 //Catalog ...
 type Catalog struct {
 	Name     string
+	Path     string
+	IsDir    bool
 	Suffix   string
 	Children []Catalog
 }
@@ -18,30 +20,57 @@ type Catalog struct {
 func main() {
 	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("static"))))
 	http.HandleFunc("/api/catalog", getCatalog)
+	http.HandleFunc("/api/doc", getDoc)
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
 func getCatalog(w http.ResponseWriter, r *http.Request) {
-	catalog := getFiles("./")
+	catalog := getFiles("root", ".")
 	json, _ := json.Marshal(catalog)
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(json)
 }
 
-func getFiles(parent string) Catalog {
+func getDoc(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	path := r.Form["path"][0]
+	dat, _ := ioutil.ReadFile(path)
+	w.Write(dat)
+}
+
+func getFiles(name string, path string) Catalog {
 	catalog := Catalog{}
-	catalog.Name = parent
-	fmt.Println(parent)
-	files, _ := ioutil.ReadDir(parent)
+	catalog.Name = name
+	catalog.Path = path
+	catalog.IsDir = true
+	files, _ := ioutil.ReadDir(path)
 	for _, f := range files {
 		subCatalog := Catalog{}
 		subCatalog.Name = f.Name()
+		subCatalog.Path = path + "/" + f.Name()
 		if f.IsDir() {
-			catalog.Children = append(catalog.Children, getFiles(f.Name()))
+			subCatalog.IsDir = true
+			subCatalogs := getFiles(subCatalog.Name, subCatalog.Path)
+			catalog.Children = append(catalog.Children, subCatalogs)
 		} else {
-			catalog.Children = append(catalog.Children, subCatalog)
+			if (strings.HasSuffix(f.Name(), "md")) {
+				subCatalog.IsDir = false
+				catalog.Children = append(catalog.Children, subCatalog)
+			}
 		}
 	}
 
 	return catalog
+}
+
+func checkCatalogHasFiles(catalog Catalog) bool {
+	if !catalog.IsDir {
+		return true
+	} else {
+		for _, c := range catalog.Children {
+			checkCatalogHasFiles(c)
+		}
+	}
+
+	return false
 }
--- src/static/js/app.js
+++ src/static/js/app.js
@@ -1,20 +1,34 @@
-(function(){
+(function () {
+    var vue;
 
-   var vue;
-    function init(){
+    function init() {
         vue = new Vue({
             el: 'body',
             data: {
-                catalog: {}
+                catalog: {},
+                doc: ""
+            },
+            methods: {
+                loadDoc: function (path) {
+                    $.post("/api/doc", {
+                        "path": path
+                    }, function (data) {
+                        vue.doc = data;
+                    });
+                }
+            },
+            filters: {
+                marked: marked
             }
+
         });
 
         loadCatalog();
     }
 
-    function loadCatalog(){
-        $.get("/api/catalog",function(data){
-          vue.catalog = data;
+    function loadCatalog() {
+        $.get("/api/catalog", function (data) {
+            vue.catalog = data;
         });
     }
 
