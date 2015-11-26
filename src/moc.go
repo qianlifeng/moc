88dd6f77a3ae118494661a871e079244734a859b
qianlifeng <qianlf2008@163.com>
2015-11-26 18:56:30 +0800

    embed html

--- src/moc.go
+++ src/moc.go
@@ -19,10 +19,26 @@ type Catalog struct {
 }
 
 func main() {
-	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("static"))))
+	http.HandleFunc("/", getStaticFile)
 	http.HandleFunc("/api/catalog", getCatalog)
 	http.HandleFunc("/api/doc", getDoc)
 	log.Fatal(http.ListenAndServe(":8081", nil))
+}
+
+func getStaticFile(w http.ResponseWriter, r *http.Request) {
+	url := r.URL.Path
+	if url == "/" {
+		url = "/index.html"
+	}
+	data, _ := Asset("static" + url)
+
+	if strings.HasSuffix(url, ".css") {
+		w.Header().Set("Content-Type", "text/css")
+	}
+	if strings.HasSuffix(url, ".html") {
+		w.Header().Set("Content-Type", "text/html")
+	}
+	w.Write(data)
 }
 
 func getCatalog(w http.ResponseWriter, r *http.Request) {
